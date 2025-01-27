abf57d6f9b559736c5a3ccac9f29b7052833a449
Arno Christie <arno.christie@gmail.com>
2025-01-27 23:26:24 +0200

    Set up backend structure and database integration.
    Created the backend folder structure.
    Defined the initial database schema and configured Prisma Client.
    Implemented basic database operations.
    Developed server setup and started creating API endpoints.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import "./App.css";
+import { eventNames } from 'process';
 
 
 type Note = {
@@ -91,11 +92,19 @@ const App = () => {
     setSelectedNote(null);
   }
 
+  const deleteNote = (event: React.MouseEvent, noteId: number) => {
+    event.stopPropagation();
+
+    const updatedNotes = notes.filter((note) => note.id != noteId);
+
+    setNotes(updatedNotes);
+  }
+
   return(<div className="app-container">
     <form className="note-form" 
       onSubmit={(event) => 
         selectedNote 
-          ? handleAddNote(event)
+          ? handleUpdateNote(event)
           : handleAddNote(event)}>
             
       <input 
@@ -131,7 +140,10 @@ const App = () => {
         <div className="note-item"
           onClick={() => handleNoteClick(note)}>
         <div className="notes-header">
-          <button>x</button>
+          <button onClick={(event) => 
+            deleteNote(event, note.id)}>
+            x
+          </button>
         </div>
         <h2>{note.title}</h2>
         <p>{note.content}</p>
